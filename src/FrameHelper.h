533252e86fb8e28a4f5aa5e9bff3c91fdba9b716
Marc Hildebrandt <marc.hildebrandt@dfki.de>
2011-03-04 12:44:25 +0100

    added calibration parameter structure

--- src/FrameHelper.h
+++ src/FrameHelper.h
@@ -3,6 +3,20 @@
 
 #include "base/samples/frame.h"
 
+#ifdef __OPENCV_ALL_HPP__
+struct CalibrationParameters
+{
+        double fx1, fy1, cx1, cy1, d01, d11, d21, d31;
+        double fx2, fy2, cx2, cy2, d02, d12, d22, d32;
+        double tx, ty, tz;
+        double rx, ry, rz;
+        cv::Mat cameraMatrix1, cameraMatrix2;
+        cv::Mat distCoeffs1, distCoeffs2;
+        cv::Mat R, T, Q;
+        cv::Mat map11, map21, map12, map22;
+};
+#endif
+
 class FrameHelper
 {
   public:
