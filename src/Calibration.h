bba704b4f13a61193143c42bc2943bb69a7c0997
Marc Hildebrandt <marc.hildebrandt@dfki.de>
2012-05-14 19:51:03 +0200

    added copy operators

--- src/Calibration.h
+++ src/Calibration.h
@@ -16,6 +16,22 @@ namespace frame_helper
     {
 	double fx, fy, cx, cy, d0, d1, d2, d3;
         int width, height;
+        /**
+         * copy to another CamCalibration structure 
+         */
+        void copyTo(CameraCalibration &target) const
+        {
+          target.fx = fx;
+          target.fy = fy;
+          target.cx = cx;
+          target.cy = cy;
+          target.d0 = d0;
+          target.d1 = d1;
+          target.d2 = d2;
+          target.d3 = d3;
+          target.width = width;
+          target.height = height;
+        }
     };
 
     /** 
@@ -31,6 +47,19 @@ namespace frame_helper
 	 * image will give the point in the right image.
 	 */
 	Eigen::Affine3d getTransform() const;
+        /**
+         * copy to another ExtrinsicCalibration structure 
+         */
+        void copyTo(ExtrinsicCalibration &target) const
+        {
+          target.tx = tx;
+          target.ty = ty;
+          target.tz = tz;
+
+          target.rx = rx;
+          target.ry = ry;
+          target.rz = rz;
+        }
     };
 
     /**
@@ -46,6 +75,15 @@ namespace frame_helper
 	 * the matlab calibration toolbox.
 	 */
 	static StereoCalibration fromMatlabFile( const std::string& file );
+        /**
+         * copy to another StereoCalibration structure 
+         */
+        void copyTo(StereoCalibration &target) const
+        {
+          camLeft.copyTo(target.camLeft);
+          camRight.copyTo(target.camRight);
+          extrinsic.copyTo(target.extrinsic);
+        }
     };
 
 }
