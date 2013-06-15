f2a222600e406b6532242c19ecb7d4e6f342810d
Fabian Franzen <flammi@watteimdocht.de>
2013-06-15 15:46:22 +0200

    Added tolerance to ExposureController

--- src/ExposureController.cpp
+++ src/ExposureController.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <cmath>
 
-LinearExposureController::LinearExposureController(int targetBrightness, int factor) {
+LinearExposureController::LinearExposureController(int targetBrightness, int factor, int tolerance) {
 	this->targetBrightness = targetBrightness;
 	this->factor = factor;
+	this->tolerance = tolerance;
 }
 void LinearExposureController::setExposureBounds(int minExposure, int maxExposure) {
 	this->upperExposureBound = maxExposure;
@@ -15,15 +16,18 @@ void LinearExposureController::setCurrentExposure(int exposure) {
 	this->currentExposure = exposure;
 }
 int LinearExposureController::getNewExposure(int currentBrightness) {
-	int diff = targetBrightness-currentBrightness;
-	int exposure_correction = diff * factor;
-	//std::cout << "Befor pow:" << exposure_correction << std::endl;
-	//exposure_correction = pow(exposure_correction * 1.0, 1.1);
-	//std::cout << "After pow:" << exposure_correction << std::endl;
-	currentExposure += exposure_correction;
+	if((currentBrightness < (targetBrightness - tolerance)) ||
+			(currentBrightness > (targetBrightness + tolerance))) {
+		int diff = targetBrightness-currentBrightness;
+		int exposure_correction = diff * factor;
+		//std::cout << "Befor pow:" << exposure_correction << std::endl;
+		//exposure_correction = pow(exposure_correction * 1.0, 1.1);
+		//std::cout << "After pow:" << exposure_correction << std::endl;
+		currentExposure += exposure_correction;
 
-	currentExposure = std::min(upperExposureBound, currentExposure);
-	currentExposure = std::max(lowerExposureBound, currentExposure);
+		currentExposure = std::min(upperExposureBound, currentExposure);
+		currentExposure = std::max(lowerExposureBound, currentExposure);
+	}
 
 	return currentExposure; 
 }
--- src/ExposureController.h
+++ src/ExposureController.h
@@ -9,8 +9,9 @@ class LinearExposureController {
 		int upperExposureBound;
 		int lowerExposureBound;
 		int currentExposure;
+		int tolerance;
 	public:
-		LinearExposureController(int targetBrightness, int factor);
+		LinearExposureController(int targetBrightness, int factor, int tolerance);
 		void setExposureBounds(int minExposure, int maxExposure);
 		void setCurrentExposure(int exposure);
 		int getNewExposure(int currentBrightness);
