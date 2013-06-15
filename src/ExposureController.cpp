c9d3da6b4f896766f8bec9afe0e3409fe39e79bb
Fabian Franzen <flammi@watteimdocht.de>
2013-06-15 12:58:42 +0200

    Added minimum/maximum ranges

--- src/ExposureController.cpp
+++ src/ExposureController.cpp
@@ -1,4 +1,7 @@
 #include <ExposureController.h>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
 
 LinearExposureController::LinearExposureController(int targetBrightness, int factor) {
 	this->targetBrightness = targetBrightness;
@@ -14,6 +17,13 @@ void LinearExposureController::setCurrentExposure(int exposure) {
 int LinearExposureController::getNewExposure(int currentBrightness) {
 	int diff = targetBrightness-currentBrightness;
 	int exposure_correction = diff * factor;
+	//std::cout << "Befor pow:" << exposure_correction << std::endl;
+	//exposure_correction = pow(exposure_correction * 1.0, 1.1);
+	//std::cout << "After pow:" << exposure_correction << std::endl;
 	currentExposure += exposure_correction;
+
+	currentExposure = std::min(upperExposureBound, currentExposure);
+	currentExposure = std::max(lowerExposureBound, currentExposure);
+
 	return currentExposure; 
 }
