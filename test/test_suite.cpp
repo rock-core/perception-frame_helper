bae15ab0f266e7099258688ef0e8d05b760b2492
Afonso da Fonseca Braga <afonso.braga@tidewise.io>
2023-07-21 11:58:07 -0300

    feat: add tests

--- test/test_suite.cpp
+++ test/test_suite.cpp
@@ -4,67 +4,88 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE "frame_helper"
 #define BOOST_AUTO_TEST_MAIN
+#include "../src/FrameHelper.h"
 #include <boost/test/auto_unit_test.hpp>
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
-#include "../src/FrameHelper.h"
-#include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <string>
 
 #include "../src/Calibration.h"
 
+using namespace std;
 using namespace base::samples::frame;
 using namespace frame_helper;
 
-//test if min max search is working
-BOOST_AUTO_TEST_CASE(convert_image)
+bool compareImages(cv::Mat src1, cv::Mat src2)
 {
-    //load image 
-    cv::Mat image =  cv::imread("test/test.jpg");
-    BOOST_CHECK(NULL != image.data);
-
-    //check if convertion is working
-    Frame frame;
-    frame.init(image.cols,image.rows,8,MODE_BGR);
-    memcpy(frame.getImagePtr(),image.ptr(),image.rows*image.cols * image.elemSize());
-    cv::Mat image2 = FrameHelper::convertToCvMat(frame);
-    cv::namedWindow("Test",cv::WINDOW_AUTOSIZE);
-    cv::imshow("Test",image2);
-    cv::waitKey(1000);
-
-    //resize image
-    Frame frame2(100,100,8,MODE_BGR);
-    FrameHelper::resize(frame,frame2);
-    cv::imshow("Test",FrameHelper::convertToCvMat(frame2));
-    cv::waitKey(2000);
-    
-    FrameHelper frame_helper;
-    frame_helper.convert(frame,frame2);
-    cv::imshow("Test",FrameHelper::convertToCvMat(frame2));
-    cv::waitKey(2000);
-    
-    //convert color
-    frame2.init(300,300,8,MODE_GRAYSCALE);
-    frame_helper.convert(frame,frame2);
-    cv::imshow("Test2",FrameHelper::convertToCvMat(frame2));
-    cv::waitKey(2000);
-
-    //convert image 
-
-//    BOOST_CHECK_EQUAL(6,result.size());
-//    BOOST_CHECK_EQUAL(0,*result[0]);
-//    BOOST_CHECK_EQUAL(5,*result[1]);
-//    BOOST_CHECK_EQUAL(0,*result[2]);
-//    BOOST_CHECK_EQUAL(0,*result[3]);
-//    BOOST_CHECK_EQUAL(6,*result[4]);
-//    BOOST_CHECK_EQUAL(0,*result[5]);
+    if (src1.rows != src2.rows) {
+        return false;
+    }
+    if (src1.cols != src2.cols) {
+        return false;
+    }
+    if (src1.channels() != src2.channels()) {
+        return false;
+    }
+    if (src1.data == src2.data) {
+        return false;
+    }
+    return true;
 }
 
+BOOST_AUTO_TEST_CASE(convert_image)
+{
+    // load image
+    cv::Mat original = cv::imread(TEST_SRC_DIR + string("/test.jpg"));
+    BOOST_CHECK(NULL != original.data);
 
-BOOST_AUTO_TEST_CASE( load_calibration_file )
+    // check if convertion is working
+    Frame frame;
+    frame.init(original.cols, original.rows, 8, MODE_BGR, -1);
+    memcpy(frame.getImagePtr(),
+        original.ptr(),
+        original.rows * original.cols * original.elemSize());
+    cv::Mat converted = FrameHelper::convertToCvMat(frame);
+    BOOST_CHECK(compareImages(original, converted));
+
+    Frame frame_resized(100, 100, 8, MODE_BGR);
+    FrameHelper::resize(frame, frame_resized);
+    cv::Mat resized = FrameHelper::convertToCvMat(frame_resized);
+
+    cv::FileStorage resized_fs(TEST_SRC_DIR + string("/resized.bin"),
+        cv::FileStorage::READ | cv::FileStorage::FORMAT_YAML);
+    cv::Mat resized_sheet;
+    resized_fs["Image"] >> resized_sheet;
+    resized_fs.release();
+    BOOST_CHECK(NULL != resized_sheet.data);
+
+    BOOST_CHECK(compareImages(resized_sheet, resized));
+
+    FrameHelper frame_helper;
+    frame_helper.convert(frame, frame_resized);
+    resized = FrameHelper::convertToCvMat(frame_resized);
+    BOOST_CHECK(compareImages(resized_sheet, resized));
+
+    // convert color
+    Frame frame_grayscale(300, 300, 8, MODE_GRAYSCALE);
+    frame_helper.convert(frame, frame_grayscale);
+    cv::Mat gray = FrameHelper::convertToCvMat(frame_grayscale);
+
+    cv::FileStorage grayscale_fs(TEST_SRC_DIR + string("/grayscale.bin"),
+        cv::FileStorage::READ | cv::FileStorage::FORMAT_YAML);
+    cv::Mat grayscale_sheet;
+    grayscale_fs["Image"] >> grayscale_sheet;
+    grayscale_fs.release();
+    BOOST_CHECK(compareImages(grayscale_sheet, gray));
+}
+
+BOOST_AUTO_TEST_CASE(load_calibration_file)
 {
     StereoCalibration calib;
-    BOOST_CHECK( !calib.isValid() );
-    calib = StereoCalibration::fromMatlabFile( "test/calib.txt", 320, 240 );
-    BOOST_CHECK( calib.isValid() );
+    BOOST_CHECK(!calib.isValid());
+    calib =
+        StereoCalibration::fromMatlabFile(TEST_SRC_DIR + string("/calib.txt"), 320, 240);
+    BOOST_CHECK(calib.isValid());
 }
